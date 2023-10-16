bb41d82779f9c1722eb3b024c092455330e65edf
Mjishu <128941980+Mjishu@users.noreply.github.com>
2023-10-15 21:11:45 -0400

    Issues in DOM Town

--- etch.js
+++ etch.js
@@ -1,5 +1,11 @@
-document.getElementById("container")
+var container = document.getElementById("container");
 
-for (let i=1; 1 < 16; i++){
+for (var i = 0; 1 < 16; i++){ 
+    var div = document.createElement("div");
+    div.setAttribute("class","new");
+    div.innerHTML = "div" + i;
+    container.appendChild(div);
 
 }
+
+// figure out why it wont create divs; line 7 is throwing an error
