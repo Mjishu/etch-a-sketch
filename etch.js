7477f64141972011ad658c4fc0dc78c8289f7a27
Mjishu <128941980+Mjishu@users.noreply.github.com>
2023-10-16 20:47:42 -0400

    got the divs to be 16x16 but still not flexible

--- etch.js
+++ etch.js
@@ -1,11 +1,14 @@
-var container = document.getElementById("container");
 
-for (var i = 0; 1 < 16; i++){ 
-    var div = document.createElement("div");
-    div.setAttribute("class","new");
-    div.innerHTML = "div" + i;
-    container.appendChild(div);
+const gridContainer = document.getElementById("grid-container");
 
+const numRows = 16;
+
+
+for (let i = 0; i < (numRows * numRows); i++){
+    const gridDiv = document.createElement("div");
+    gridDiv.className = "gridDiv"
+    gridContainer.appendChild(gridDiv);
+    
 }
 
 // figure out why it wont create divs; line 7 is throwing an error
